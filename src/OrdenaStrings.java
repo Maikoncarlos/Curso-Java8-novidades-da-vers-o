4503a007a37e9afa7f4f7353b4a934751f006d19
Maikoncarlos <maikonsaturno@gmail.com>
2023-04-08 19:15:26 -0300

    que venham os lambdas

--- src/OrdenaStrings.java
+++ src/OrdenaStrings.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class OrdenaStrings {
     public static void main(String[] args) {
@@ -11,40 +10,85 @@ public class OrdenaStrings {
         palavras.add("editora casa do código");
         palavras.add("caelum");
 
-        Comparator<String> compador = new ComparadorPorTamanho();
 
+//        Comparator<String> compador = new ComparadorPorTamanho();
 //        Collections.sort(palavras, compador); modo antigo!
-        palavras.sort(compador);
+
+
+//        palavras.sort(new Comparator<String>() {
+//            @Override
+//            public int compare(String palavra1, String palavra2) {
+//                if(palavra1.length() < palavra2.length())
+//                    return -1;
+//                if(palavra1.length() > palavra2.length())
+//                    return 1;
+//                return 0;
+//            }
+//        }); modo antigo!
+
+        //METODO PARA ORDENAR DA STRING MENOR PARA A MAIOR:
+
+        //palavras.sort((p1, p2) -> Integer.compare(p1.length(), p2.length())); //FORMA HABITUAL
+        palavras.sort(Comparator.comparingInt(String::length)); //Forma Oculta
         System.out.println(palavras);
         System.out.println("###############");
+
 
 //        for (String p : palavras){
 //            System.out.println(p);
 //        } modo antigo!
 
-        Consumer<String> consumidor = new ImprimeNaLinha();
-            palavras.forEach((p) -> System.out.println(p));
-            palavras.forEach(System.out::println); //igual ao de cima!
+//        Consumer<String> consumidor = new ImprimeNaLinha();
+//            palavras.forEach(consumidor); //modo antgo!
+        palavras.forEach((p) -> System.out.println(p));
+        palavras.forEach(System.out::println); //igual ao de cima!
 
     }
 }
 
-class ImprimeNaLinha implements Consumer<String>{
+//class ImprimeNaLinha implements Consumer<String>{
+//
+//    @Override
+//    public void accept(String s) {
+//        System.out.println(s);
+//    }
+//}
 
-    @Override
-    public void accept(String s) {
-        System.out.println(s);
+//class ComparadorPorTamanho implements Comparator<String>{
+//
+//    @Override
+//    public int compare(String palavra1, String palavra2) {
+//        if(palavra1.length() < palavra2.length())
+//            return -1;
+//        if(palavra1.length() > palavra2.length())
+//            return 1;
+//        return 0;
+//    }
+//}
+
+class OrdenaStringsLambda {
+    public static void main(String[] args) {
+
+        List<String> palavras = new ArrayList<>();
+        palavras.add("alura online");
+        palavras.add("editora casa do código");
+        palavras.add("caelum");
+
+        System.out.println("ANTES DO SORT");
+//        palavras.forEach(p -> System.out.println(p));
+        palavras.forEach(System.out::println);
+        System.out.println();
+
+//        palavras.sort((p1, p2) -> Integer.compare(p1.length(), p2.length())); modo trivial !
+        palavras.sort(Comparator.comparingInt(String::length));
+
+        System.out.println("DEPOIS DO SORT");
+//        palavras.forEach(p -> System.out.println(p));
+        palavras.forEach(System.out::println);
+        System.out.println();
+
+        // Consumer espera uma expressão lambda e retorna void =  s -> {};
+        // Comparator espera dois argumentos e retorna um Integer (s1, s2) -> {};
     }
 }
 
-class ComparadorPorTamanho implements Comparator<String>{
-
-    @Override
-    public int compare(String palavra1, String palavra2) {
-        if(palavra1.length() < palavra2.length())
-            return -1;
-        if(palavra1.length() > palavra2.length())
-            return 1;
-        return 0;
-    }
-}
